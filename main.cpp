889e9b33ccb8a779bfaf47b4b90bbb0665798d8a
khubaib321 <khubaib.afzal11@gmail.com>
2024-10-31 05:05:11 +0100

    Notepad--

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <cctype>
+#include <vector>
 #include <iostream>
 #include "lib/Dictionary.h"
 
@@ -22,7 +24,7 @@ int main() {
     cout << "Loaded " << dict.getWordCount() << " words." << endl;
 
     const char* searchWords[] = {
-        "donut",
+        "gess",
         "manga",
         "mango",
         "nerdy",
@@ -33,6 +35,34 @@ int main() {
 
     for (const char* word : searchWords) {
         cout << word << ' ' <<  dict.searchWord(word) << endl;
+    }
+
+    cout << endl << "Welcome to the world's \"fastest\" notepad. Type away! (press esc+enter to escape)" << endl << endl;
+
+    string word = "";
+    vector<string> words;
+
+    while (true) {
+        const char ch = cin.get();
+
+        if (ch == 27) {
+            break;
+        }
+
+        if (isalnum(ch)) word += ch;
+        else {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+    }
+    
+    cout << endl << "Your english:" << endl;
+    for (const auto& w : words) {
+        cout << w << ' ';
+        if (dict.searchWord(w.c_str())) cout << "...ok" << endl;
+        else cout << "...lol" << endl;
     }
 
     return 0;
